95907f5e95237c6fe303704b15d2a7e4a05442dd
Marcos Iglesias <marcosiglsdz@gmail.com>
2022-02-13 23:03:19 +0100

    Region: Migration,seed,model,test,list of regions

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             GenderSeeder::class,
             CountrySeeder::class,
+            RegionSeeder::class,
         ]);
     }
 }
--- routes/api_v1.php
+++ routes/api_v1.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\API\V1\Auth\RegisterController;
 
 use App\Http\Controllers\API\V1\CountryController;
 use App\Http\Controllers\API\V1\GenderController;
+use App\Http\Controllers\API\V1\RegionController;
 use App\Http\Controllers\API\V1\UserProfileController;
 
 /*
@@ -30,4 +31,5 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::put('user/profiles', [UserProfileController::class, 'updateMyProfile']);
 
     Route::get('countries', [CountryController::class, 'index']);
+    Route::get('regions', [RegionController::class, 'index']);
 });
